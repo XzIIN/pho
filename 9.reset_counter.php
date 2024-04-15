ff0ac188f5658c847d895e4d787ed6d3f1544188
XzIIN <135249265+XzIIN@users.noreply.github.com>
2024-04-15 12:10:52 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+    session_start(); //啟動 PHP 會話
+    unset($_SESSION["counter"]); //刪除會話中的 "counter" 變數
+    echo "counter重置成功...."; //輸出重置成功的消息
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>"; //使用 meta 標籤在 2 秒後重新定向到指定的 URL
 
 ?>
