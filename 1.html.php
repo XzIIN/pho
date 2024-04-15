ad0cd7374ebc2ea1899f02af87d66ee8445c57c8
XzIIN <135249265+XzIIN@users.noreply.github.com>
2024-04-15 11:10:48 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,10 +1,10 @@
 <html>
-    <head><title>html+php</title></head>      
+    <head><title>html+php</title></head> //設置標題為html+php
     <body>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
+        <?php echo "Hello world!"; ?> //在網頁中顯示 "Hello world!"
+        <h1>html</h1> //創建一個一級標題，標題內容為 "html"
+        <?php echo "Hello world!"; ?> //在網頁中顯示 "Hello world!"
+        <h1>html</h1> //創建一個一級標題，標題內容為 "html"
+        <?php echo "Hello world!"; ?> //在網頁中顯示 "Hello world!"
     </body>
 </html>
