43709fecc4506126728dd30de7821b0373b3f40b
XzIIN <135249265+XzIIN@users.noreply.github.com>
2024-04-15 11:16:04 +0800

    Update 2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,5 @@
 <?php 
-    echo $_POST["id"];
-    echo "<br>";
-    echo $_POST["pwd"];
+    echo $_POST["id"]; //將從 POST 請求中獲取到的 "id" 參數的值輸出到瀏覽器上
+    echo "<br>"; //在瀏覽器中插入一個斷行，使後續的內容從下一行繼續顯示
+    echo $_POST["pwd"]; //將從 POST 請求中獲取到的 "pwd" 參數的值輸出到瀏覽器上
 ?>
