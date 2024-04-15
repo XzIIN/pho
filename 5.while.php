324faaf1485245bb95cb5ccb40112d1c836acab8
XzIIN <135249265+XzIIN@users.noreply.github.com>
2024-04-15 11:41:06 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,10 +1,10 @@
 <?php
-   #mysqli_connect() 建立資料庫連結 ////建立與 MySQL 數據庫的連接的函數
-   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-   #mysqli_query() 從資料庫查詢資料 ////mysqli_query() 函數來向 MySQL 數據庫發送一個查詢
-   $result=mysqli_query($conn, "select * from user");
-   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-   while ($row=mysqli_fetch_array($result)) {
-     echo $row["id"]." ".$row["pwd"]."<br>";
+   #mysqli_connect() 建立資料庫連結 //建立與 MySQL 數據庫的連接的函數
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust"); //
+   #mysqli_query() 從資料庫查詢資料 //mysqli_query() 函數來向 MySQL 數據庫發送一個查詢
+   $result=mysqli_query($conn, "select * from user"); //
+   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來 //從查詢結果集中逐行獲取數據的函數
+   while ($row=mysqli_fetch_array($result)) { //
+     echo $row["id"]." ".$row["pwd"]."<br>"; //
    } 
 ?>
