4c6c425867263ac0c744860b018f1d30141f15ab
XzIIN <135249265+XzIIN@users.noreply.github.com>
2024-04-15 12:05:26 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,8 +1,8 @@
 <?php
-    error_reporting(0);
-    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-    $result=mysqli_query($conn, "select * from bulletin");
-    echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
+    error_reporting(0); //關閉錯誤報告
+    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust"); //建立與 MySQL 數據庫的連接
+    $result=mysqli_query($conn, "select * from bulletin"); //執行 SQL 查詢，從 "bulletin" 表中選擇所有的列和行
+    echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>"; //輸出 HTML 表格的開始標籤和表頭
     while ($row=mysqli_fetch_array($result)){
         echo "<tr><td>";
         echo $row["bid"];
@@ -15,6 +15,6 @@
         echo "</td><td>";
         echo $row["time"];
         echo "</td></tr>";
-    }
-    echo "</table>"
+    } //遍歷查詢結果集中的每一行，並將每一行的值輸出到 HTML 表格中
+    echo "</table>" //將 HTML 表格的結束標籤輸出到瀏覽器上，以完成 HTML 表格的構建
 ?>
