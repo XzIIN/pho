2b91766885a2a36a5d14cfc12e932ab43b30a517
XzIIN <135249265+XzIIN@users.noreply.github.com>
2024-04-15 11:19:33 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,5 +1,5 @@
 <?php 
-    if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234"))
+    if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234")) //如果從 POST 請求中獲取的 "id" 參數的值等於 "john"，且 "pwd" 參數的值等於 "john1234"，則執行下一段程式碼
         echo "登入成功";
     else
         echo "登入失敗";
